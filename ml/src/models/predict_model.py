3b8fc04c6becc8323b6f4d4970cf3eb667b13dac
Nefertiti23 <fatima.xx.umar@gmail.com>
2026-05-16 00:25:55 +0500

    Merge ml/ folder updates from frontend branch

--- ml/src/models/predict_model.py
+++ ml/src/models/predict_model.py
@@ -13,32 +13,23 @@ def predict_proba(model, X):
 if __name__ == "__main__":
     from train_models import get_dataset, make_features
 
-    model = load_model("models/random_forest.joblib")
+    model = load_model("models/xgboost.joblib")
     
 #     # df = get_dataset("data/processed/final_dataset.csv")
 #     X, _ = make_features(df)
 
-    sample = pd.DataFrame([{
-        'child_age_months': 53.0,
-        'child_sex': 0,
-        'breastfeeding': 2.0,
-        'haz_score': -5.66,
-        'waz_score': -4.71,
-        'whz_score': -2.71,
-        'underweight': 0,
-        'wasted': 0,
-        'mother_education': 1.0,
-        'urban_rural': 0,
-        'district': 5.0,
-        'wealth_index': 1.0,
-        'disability': 0.0,
-        'province': 2,
-        'birth_weight_known':0,
-        'housing_quality': 2.0,
-        'water_source': 31.0,
-        'sanitation_type': 1.0,
-        'education_level': 0.0,
-        'household_education': 0.0,
-    }])
+    sample = pd.DataFrame([
+        {'child_age_months': 12, 'mother_education': 0, 'wealth_index': 1, 
+         'urban_rural': 1, 'province': 0},
+        {'child_age_months': 18, 'mother_education': 0, 'wealth_index': 1, 
+          'urban_rural': 1, 'province': 0},
+        {'child_age_months': 48, 'mother_education': 3, 'wealth_index': 5, 
+         'urban_rural': 0, 'province': 1},
+        {'child_age_months': 36, 'mother_education': 2, 'wealth_index': 3, 
+         'urban_rural': 0, 'province': 2},
+        {'child_age_months': 24, 'mother_education': 1, 'wealth_index': 2, 
+         'urban_rural': 1, 'province': 1}
+])
     preds = predict(model, sample)
-    print("Stunted:", bool(preds[0]))
+    for pred in preds:
+        print("Stunted:", bool(pred))
