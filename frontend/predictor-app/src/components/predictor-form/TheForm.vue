fae4ffda1868422e88efd918ef0beca7db2e4d51
Nefertiti23 <fatima.xx.umar@gmail.com>
2026-05-16 00:11:10 +0500

    initial vue setup

---
+++ frontend/predictor-app/src/components/predictor-form/TheForm.vue
@@ -0,0 +1,12 @@
+<script>
+import './Form.css'
+</script>
+
+<template>
+  <form>
+    <p>Add item</p>
+    <p>Item name: <input type="text" required></p>
+    <p>How many: <input type="number"></p>
+    <button type="submit">Add item</button>
+  </form>
+</template>
