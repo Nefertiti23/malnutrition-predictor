058066af81a5b8be2269bff7245daa5ec070eade
Nefertiti23 <fatima.xx.umar@gmail.com>
2026-05-16 01:30:22 +0500

    form initial set up

--- frontend/predictor-app/src/components/predictor-form/TheForm.vue
+++ frontend/predictor-app/src/components/predictor-form/TheForm.vue
@@ -4,9 +4,32 @@ import './Form.css'
 
 <template>
   <form>
-    <p>Add item</p>
-    <p>Item name: <input type="text" required></p>
-    <p>How many: <input type="number"></p>
-    <button type="submit">Add item</button>
+    <div class="input-box">
+        <label>Child Age (in months)</label>
+        <input type="number" required>
+    </div>
+    <div class="divider-line"></div>
+    <div class="input-box">
+        <label>Mother's Education</label>
+        <p>On a scale of 1 - 5</p>
+        <input type="number" required>
+    </div>
+    <div class="divider-line"></div>
+    <div class="input-box">
+        <label>Wealth Index</label>
+        <p>On a scale of 1 - 5</p>
+        <input type="number" required>
+    </div>
+    <div class="divider-line"></div>
+    <div class="input-box">
+        <label>District Type</label>
+        <p>Urban or rurle</p>
+        <select id="district_type" name="district_type">
+            <option value=0>Rural</option>
+            <option value=1>Urban</option>
+        </select>
+    </div>
+    <div class="divider-space"></div>
+    <button type="submit">Make Prediction</button>
   </form>
 </template>
