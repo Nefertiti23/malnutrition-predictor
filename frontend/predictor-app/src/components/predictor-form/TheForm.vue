916142aa43e4f09c86eb4f6baab12ecab6d073fd
Nefertiti23 <fatima.xx.umar@gmail.com>
2026-05-17 15:23:39 +0500

    only form ui

--- frontend/predictor-app/src/components/predictor-form/TheForm.vue
+++ frontend/predictor-app/src/components/predictor-form/TheForm.vue
@@ -23,10 +23,19 @@ import './Form.css'
     <div class="divider-line"></div>
     <div class="input-box">
         <label>District Type</label>
-        <p>Urban or rurle</p>
+        <p>Urban or rural?</p>
         <select id="district_type" name="district_type">
             <option value=0>Rural</option>
             <option value=1>Urban</option>
+        </select>
+    </div>
+    <div class="divider-line"></div>
+    <div class="input-box">
+        <label>Province</label>
+        <select id="province" name="province">
+            <option value=0>Sindh</option>
+            <option value=1>Balochistan</option>
+            <option value=2>KPK</option>
         </select>
     </div>
     <div class="divider-space"></div>
