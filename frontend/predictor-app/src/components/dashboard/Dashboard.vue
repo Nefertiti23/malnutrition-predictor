ec8dfdda8f143ff0e733fed86159d455043806f6
Nefertiti23 <fatima.xx.umar@gmail.com>
2026-05-20 00:35:30 +0500

    Revert

--- frontend/predictor-app/src/components/dashboard/Dashboard.vue
+++ frontend/predictor-app/src/components/dashboard/Dashboard.vue
@@ -81,8 +81,8 @@ const wealthLabels = {
 
 onMounted(async () => {
   try {
-    const apiUrl = import.meta.env.VITE_API_URL || '/api'
-    const response = await fetch(`${apiUrl}/api/dashboard-stats`);
+    const apiUrl = import.meta.env.VITE_API_URL || '/'
+    const response = await fetch(`${apiUrl}/dashboard-stats`);
     const json = await response.json();
     
     if (json.status === 'success') {
